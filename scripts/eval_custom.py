7e9c0cd0d0b64002841e0ea26da364291b9a489a
ChangyongShin <scy3942@gmail.com>
2026-04-02 15:11:43 +0900

    Update eval_custom.py

--- scripts/eval_custom.py
+++ scripts/eval_custom.py
@@ -154,7 +154,7 @@ class Evaluator:
             )
         ]
         # Update observation modalities
-        cfg["robots"][0]["obs_modalities"] = ["proprio", "rgb"]
+        cfg["robots"][0]["obs_modalities"] = ["proprio", "rgb", "seg_instance"]
         cfg["robots"][0]["proprio_obs"] = list(PROPRIOCEPTION_INDICES["R1Pro"].keys())
         if self.cfg.robot.controllers is not None:
             cfg["robots"][0]["controller_config"].update(self.cfg.robot.controllers)
